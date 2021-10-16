737c5ca0d4f76e74d615bac236fcc2787caec70b
Abhishek Verma <akv.abd@gmail.com>
2021-10-16 15:46:56 +0530

    cron job with example

--- reusablecode.js
+++ reusablecode.js
@@ -171,3 +171,80 @@ worksheet.sort((a,b) => new moment(a[5]).format('YYYYMMDD') - new moment(b[5]).f
 worksheet.sort((a,b) => a[5] > b[5])
 }
 
+13. Compare two arrays
+
+var arr1 = [7720,765,762,759,757,755,753,568,502,482,427,407,403,402,395,390,385,376,374,369,368,352,351,263,262,259,257,253,251,250,246,239,238,237,235,234,2324,232,231,2299,229,228,227,2250,2246,2245,2235,2234,2232,211,195,194,192,189,183,182,181,167,162,159,158,155,14950,149,1438,1402,1392,1391,118];
+var arr2 = [118,1391,1392,1402,1438,149,14950,155,158,159,162,167,181,182,183,189,192,194,195,211,2232,2234,2235,2245,2246,2250,227,228,229,2299,231,232,2324,234,235,237,238,239,246,250,251,253,257,259,262,263,351,352,368,369,374,376,385,390,395,402,403,407,427,482,502,568,753,755,757,759,762,765,772];
+//check lengths
+
+function areEqual(arr1,arr2){
+var len1 = arr1.length;
+var len2 = arr2.length;
+console.log('arr1.length : '+arr1.length+'& arr2.length : '+arr2.length);
+if(arr1.length == arr2.length){
+console.log('Equal Lengths');
+arr1.sort();
+arr2.sort();
+console.log(arr1);
+console.log(arr2);
+var flag = true;
+for(var i= 0;i<arr1.length;i++){
+if(arr1[i] !== arr2[i]){
+console.log('Different elements found at index :'+i);
+console.log('arr1[i] : '+arr1[i]+' & arr2[i] : '+arr2[i]);
+flag = false;
+break;
+}
+}
+if(flag){
+console.log('Order of elements is same');
+}
+}else{
+console.log('Unequal lengths');
+console.log(arr_diff(arr1,arr2));
+}
+}
+areEqual(arr1,arr2);
+
+function arr_diff(a1, a2) {
+var a = [], diff = [];
+for (var i = 0; i < a1.length; i++) {
+a[a1[i]] = true;
+}
+for (var i = 0; i < a2.length; i++) {
+if (a[a2[i]]) {
+delete a[a2[i]];
+} else {
+a[a2[i]] = true;
+}
+}
+for (var k in a) {
+diff.push(k);
+}
+return diff;
+}
+
+14. CRON job schedule format
+
+The Cron time string is five values separated by spaces, based on the following information:
+(For Indian time, assume 5:30 is automatically added to whatever you schedule)
+Character Descriptor Acceptable values
+1 Minute 0 to 59, or * (no specific value)
+2 Hour 0 to 23, or * for any value. All times UTC.
+3 Day of the month 1 to 31, or * (no specific value)
+4 Month 1 to 12, or * (no specific value)
+5 Day of the week 0 to 7 (0 and 7 both represent Sunday), or * (no specific value)
+
+Note that entering a value for a character attribute configures a task to run at a regular time. If you append a slash ( / )
+and an integer to a wildcard in any of the character positions however, you can configure the cron task to run at a regular interval that isn't dependent
+on a regular time (for example, every X minutes/hours/days)./
+
+i.e.
+// only once per hour
+30 * * * * Execute a command at 30 minutes past the hour, every hour.
+// multiple times per hour
+*/5 * * * * Execute a command every five minutes./
+
+More Info : https://www.thegeekstuff.com/2009/06/15-practical-crontab-examples
+
+
